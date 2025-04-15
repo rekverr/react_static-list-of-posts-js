82ec49c074a2a7c0251fee2b3df1fcd1e0eb9729
Viktor Shchehelskyi <victorshegelsky@gmail.com>
2025-04-15 03:19:09 +0300

    redid

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -5,7 +5,9 @@ export const CommentList = ({ comments }) =>
   comments.length > 0 && (
     <div className="CommentList">
       {comments.map(comment => (
-        <CommentInfo comment={comment} key={comment.id} />
+        <div key={comment.id}>
+          <CommentInfo comment={comment} />
+        </div>
       ))}
     </div>
   );
